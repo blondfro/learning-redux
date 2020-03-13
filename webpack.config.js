1f7eb257d5a2faea816e8dc956718d090f9a7a5e
James Morris <blondfro@gmail.com>
2020-03-13 14:15:17 -0500

    transferred react project from exercise files and update package.json, babelrc, webpack.config and src/index to account for changes.

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
 module.exports = {
     entry: "./src/index.js",
     output: {
@@ -17,14 +19,31 @@ module.exports = {
                 exclude: /(node_modules)/,
                 loader: ['babel'],
                 query: {
-                    presets: ['latest', 'stage-0']
+                    presets: ['latest', 'stage-0', 'react']
                 }
             },
             {
                 test: /\.json$/,
                 exclude: /(node_modules)/,
                 loader: 'json-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader!autoprefixer-loader'
+
+            },
+            {
+                test: /\.scss/,
+                loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader'
             }
         ]
-    }
-}
+    },
+    plugins: [
+        new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.optimize\.css$/g,
+            cssProcessor: require('cssnano'),
+            cssProcessorOptions: {discardComments: {removeAll: true}},
+            canPrint: true
+        })
+    ]
+};
