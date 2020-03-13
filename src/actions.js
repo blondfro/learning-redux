780126d82e278efcd500c20f3678804354ac0c38
James Morris <blondfro@gmail.com>
2020-03-13 12:26:36 -0500

    created more action creators.

--- src/actions.js
+++ src/actions.js
@@ -1,4 +1,5 @@
 import constants from "./constants";
+import {suggestions} from "./store/reducers/reducers";
 
 export function addDay(resort, date, powder=false, backcountry=false) {
     return {
@@ -18,5 +19,32 @@ export function setGoal(goal) {
     return {
         type: constants.SET_GOAL,
         payload: goal
+    }
+}
+
+export function addError(error) {
+    return {
+        type: constants.ADD_ERROR,
+        payload: error
+    }
+}
+
+export function clearError(index) {
+    return {
+        type: constants.CLEAR_ERROR,
+        payload: index
+    }
+}
+
+export function changeSuggestions(suggestions) {
+    return {
+        type: constants.CHANGE_SUGGESTIONS,
+        payload: suggestions
+    }
+}
+
+export function clearSuggestions() {
+    return {
+        type: constants.CHANGE_SUGGESTIONS
     }
 }
