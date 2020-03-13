39de8f24d3911bd479db81852dc9fcde3093eac8
James Morris <blondfro@gmail.com>
2020-03-13 15:50:33 -0500

    modified the add day container.

--- src/components/containers/AddDayForm.js
+++ src/components/containers/AddDayForm.js
@@ -1,12 +1,37 @@
-import AddDayForm from '../ui/AddDayForm'
-import { withRouter } from 'react-router'
+import AddDayForm from '../ui/AddDayForm';
+import { withRouter } from 'react-router';
+import {
+    addDay,
+    clearSuggestions,
+    suggestResortNames
+} from "../../actions";
+import { connect } from "react-redux";
+
+const mapStateToProps = (state, props) => ({
+   suggestions: state.resortNames.suggestions,
+   fetching: state.resortNames.fetching,
+   router: props.router
+});
+
+const mapDispatchToProps = dispatch => ({
+    onNewDay({resort, date, powder, backcountry}) {
+        dispatch(addDay(resort, date, powder, backcountry))
+    },
+    onChange(value) {
+        value
+            ? dispatch(suggestResortNames(value))
+            : dispatch(clearSuggestions())
+    },
+    onClear() {
+        dispatch(clearSuggestions())
+    }
+});
+
+
+
+const Container = connect(mapStateToProps, mapDispatchToProps)(AddDayForm);
 
 export default withRouter(
     (props) => 
-        <AddDayForm suggestions={[]} 
-                fetching={false} 
-                router={props.router} 
-                onNewDay={day => console.log('todo: add day', day)}
-                onChange={value => console.log('todo: suggest', value)}
-                onClear={() => console.log('todo: clear suggestions')} />
+        <Container />
 )
